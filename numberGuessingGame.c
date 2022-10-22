2175d3b375d04f2244adcf98932cf1ecbef179c3
AndrewJohnValdez <andrewjohnv@cpp.edu>
2022-10-22 01:05:14 -0700

    numberGuessingGame outline

---
+++ numberGuessingGame.c
@@ -0,0 +1,79 @@
+/*
+Create an interactive program
+    allows user to guess a number between 1-10 (default)
+    have main menu of up to 3 choices
+        1 - play game 
+        2 - ability to change value limit but cannot go past MAX VALUE
+            Note:
+                must be able to back out of this side menu
+        3 - quit
+            thank user for the program ends
+
+Create a new branch once program is completed
+save_user_max_number
+    remember the user's chosen max value for the next time the game starts (dynamic linking?)
+*/
+
+/*
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
+
+
+void generator(int num) {
+
+    int number;
+    int userInput;
+
+    //possibly record number of guesses?
+
+    //random number generator
+    srand(time(NULL));
+
+    number = rand() % num;
+
+    //if statements depending on the user guess and if correct or wrong
+
+    //Create an if statement regarding invalid guesses (going past max_value or base_value or letters/decimals)
+
+}
+
+
+int main() 
+{
+
+//int userChoice;
+//int base_value = 10;
+//int max_value = 100
+
+//print a main menu at start
+
+
+//read information from console
+
+
+//prompt menu with the 3 options (switch case)
+//scanf provided to read input and if statement to display next part
+
+
+
+//allow user to change base_value, be from case(2)
+
+
+//call method for game functions
+generator(n)
+
+
+return EXIT_SUCCESS;
+}
+
+
+*/
+
+/*
+Must create a branch that will save the user's input value
+
+dynamic linking possible?
+*/
